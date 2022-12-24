9a2cc3481732169b5d30b77d3c3581505368bec6
juned aslam <junedaslam96@gmail.com>
2022-12-24 19:54:30 +0530

    commited by juned

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
+SECRET_KEY = 'django-insecure-azfok_ol^m-pwmt@dtlp1x%v4a#ibq+pk8@5e92c3**p*zk@xp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["junedportfolio.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["*","localhost"]
 
 
 # Application definition
@@ -118,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT =BASE_DIR / 'static'
 
 # Default primary key field type
